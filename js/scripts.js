7557fe326998fdccee91c219efa9806c88ccc84f
koros99 <koronje254@gmail.com>
2019-02-12 12:35:36 +0300

    Add addition function from JSFidle

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var add = function (number1, number2){
+return number1 + number2;
+}
+var number1= parseInt(prompt("Enter number 1"));
+var number2= parseInt(prompt("Enter number 2"));
+alert(add(number1,number2));
